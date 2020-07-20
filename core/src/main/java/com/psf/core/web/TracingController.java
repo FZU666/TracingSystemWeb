d0da1384dc3a5ead8783b5cea341fc41300e024f
fuqijia <60819777+fuqijia@users.noreply.github.com>
2020-07-20 22:07:15 +0800

    qijia

--- core/src/main/java/com/psf/core/web/TracingController.java
+++ core/src/main/java/com/psf/core/web/TracingController.java
@@ -1,14 +1,26 @@
 package com.psf.core.web;
 
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.json.JSON;
+import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
+import com.psf.core.mapper.ResultMapper;
+import com.psf.core.mapper.TargetMapper;
+import com.psf.core.model.Result;
+import com.psf.core.model.Target;
+import com.psf.core.model.TargetExample;
 import com.psf.core.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -23,7 +35,61 @@ public class TracingController {
 
     @Autowired
     private StorageService storageService;
+    //***********************************其佳的部分
+    @Autowired
+    private TargetMapper targetMapper;
+    @Autowired
+    private ResultMapper resultMapper;
 
+
+    @GetMapping("/imageurl")
+    @ResponseBody
+    public ResponseEntity imageurl()
+    {
+        TargetExample targetExample=new TargetExample();
+        List<Target> t=targetMapper.selectByExampleWithBLOBs(targetExample);
+        JSONArray jsonArray=new JSONArray();
+        for(Target target:t)
+        {
+            JSONObject jsonObject=new JSONObject();
+            String tid=target.getTid().toString();
+            String imageurl=target.getImgurl();
+            jsonObject.put(tid,imageurl);
+            jsonArray.add(jsonObject);
+        }
+        return ResponseEntity.ok().body(jsonArray);
+    }
+
+    @GetMapping("/eigenvalue")
+    @ResponseBody
+    public ResponseEntity<List<Target>> eigenvalue()
+    {
+        TargetExample targetExample=new TargetExample();
+        List<Target> t=targetMapper.selectByExampleWithBLOBs(targetExample);
+        return ok().body(t);
+    }
+
+    @PostMapping("/result")
+    @ResponseBody
+    public void result(Integer id, String rid, Integer tid, Date discoverytime)
+    {
+        Result result=new Result();
+        result.setDiscoverytime(discoverytime);
+        result.setId(id);
+        result.setRid(rid);
+        result.setTid(tid);
+        resultMapper.insert(result);
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder, WebRequest request) {
+
+        //转换日期
+        DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));// CustomDateEditor为自定义日期编辑器
+    }
+
+    //***************************其佳的部分
     /*
      *
      *
